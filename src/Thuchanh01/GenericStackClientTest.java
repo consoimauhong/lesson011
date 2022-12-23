4eb73395c00a6993fd47d87b17214fd338ed6e8d
Your Name <you@example.com>
2022-12-23 09:14:07 +0700

    new

---
+++ src/Thuchanh01/GenericStackClientTest.java
@@ -0,0 +1,10 @@
+package Thuchanh01;
+
+public class GenericStackClientTest {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        GenericStackClient.stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        GenericStackClient.stackOfIStrings();
+    }
+}
---
+++ src/Thuchanh01/MyGenericStack.java
@@ -0,0 +1,36 @@
+package Thuchanh01;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack <T>{
+    private LinkedList<T> stack;
+
+    public MyGenericStack(){
+        stack = new LinkedList();
+    }
+    //cai dat phuong thuc push
+    public void push (T element){
+        stack.addFirst(element);
+    }
+    //cai dat phuong thuc pop
+    public T pop(){
+        if (isEmpty()){
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+    // cai dat phuong thuc size()
+    public int size(){
+        return stack.size();
+    }
+    //cai dat phuong thuc isEmpty()
+    public boolean isEmpty(){
+        if(stack.size()== 0){
+            return true;
+        }
+        return false;
+    }
+}
+
+
